a4ffe00d6d4a35922cff4bff3183a9c0279b99a0
joaot1 <joaotombinibortolotti@Gmail.com>
2024-10-02 21:35:02 -0300

    Codigos

--- src/atividade01.java
+++ src/atividade01.java
@@ -1,4 +1,40 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class atividade01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantos filmes você deseja avaliar?");
+        int qtdFilmes = sc.nextInt();
+
+        double[] notas = new double[qtdFilmes];
+        int filmesbons = 0;
+        int filmesruins = 0;
+        double somaNotas = 0;
+
+
+        for(int i = 0; i < qtdFilmes; i++){
+            System.out.println("Digite a nota do filme " + (i+1) + ": ");
+            notas[i] = sc.nextDouble();
+
+
+            if(notas[i] <0 || notas[i] > 10){
+                System.out.println("Digite a nota do filme de 0 a 10");
+                i--;
+            }else{
+                notas[i] = notas[i] / 10;
+                somaNotas += notas[i];
+            }if(notas[i] >= 6){
+                filmesruins++;
+            }else{
+                filmesbons++;
+            }
+
+            double media = somaNotas / qtdFilmes;
+        }
+
+        // Saída das avaliações dos filmes
+        System.out.println("A quantidade de filmes avaliados em bons " + filmesbons);
+        System.out.println("A quantidade de filmes avaliados em ruins " + filmesruins);
+    }
 }
+
--- src/atividade02.java
+++ src/atividade02.java
@@ -1,4 +1,51 @@
-package PACKAGE_NAME;
-
+import java.util.Scanner;
 public class atividade02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Quantidades de pessoas que vão votar.");
+        int pessoas = sc.nextInt();
+
+        double[] votos = new double[pessoas];
+        int votosTaffe = 0;
+        int votosRodrigo = 0;
+        int votosLucas = 0;
+        int votosAlessandro = 0;
+
+        for(int i = 0; i < pessoas; i++){
+            System.out.println("Qual das opções você deseja votar? ");
+            System.out.println("taffe - 99");
+            System.out.println("rodrigo - 88");
+            System.out.println("lucas - 77");
+            System.out.println("Alessandro - 66");
+            int opcao = sc.nextInt();
+
+            if(opcao == 99){
+                votos[i] = 99;
+                votosTaffe++;
+            }if(opcao == 88){
+                votos[i] = 88;
+                votosRodrigo++;
+            }if(opcao == 77){
+                votos[i] = 77;
+                votosLucas++;
+            }if (opcao == 66) {
+                votos[i] = 66;
+                votosAlessandro++;
+            }
+        }
+            if(votosTaffe > votosRodrigo && votosTaffe > votosLucas && votosTaffe > votosAlessandro){
+                System.out.print("Taffe ganhou " + votosTaffe++ + " votos");
+            }else if(votosRodrigo > votosTaffe && votosRodrigo > votosLucas && votosRodrigo > votosAlessandro){
+                System.out.print("Rodrigo ganhou " + votosRodrigo++ + " votos");
+            }else if(votosLucas > votosTaffe && votosLucas > votosRodrigo && votosLucas > votosAlessandro){
+                System.out.print("Lucas ganhou " + votosLucas++ + " votos");
+            }else if(votosAlessandro > votosTaffe && votosAlessandro > votosRodrigo && votosAlessandro > votosLucas){
+                System.out.print("Alessandro ganhou " + votosAlessandro++ + " votos");
+            }else{
+                System.out.print("Vote em um dos numeros do candidato");
+            }
+
+
+    }
 }
